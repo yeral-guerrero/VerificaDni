730532de71bbd0ffe8cb24f5ef331b7612d53b91
@yeral-guerrero <gagl73@yahoo.com>
2020-06-04 15:00:12 +0200

    Verfica DNI

---
+++ js/verificadni.js
@@ -0,0 +1,8 @@
+var dni=document.getElementById("dni");
+console.log(dni);
+console.log(dni.innerHTML)
+
+var btn_submit = document.getElementById("submit")
+btn_submit.addEventListener("click", function () {
+    alert("DNI Incorrecto");
+  });
