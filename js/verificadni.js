67be7cb7ea2b3b813fee4af2e78a68d39f8ea9dc
@yeral-guerrero <gagl73@yahoo.com>
2020-06-04 21:28:20 +0200

    Verfica DNI

--- js/verificadni.js
+++ js/verificadni.js
@@ -1,14 +1,19 @@
+const divisor = 23;
+var letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
 
 var nombre01=document.getElementById("nombre");
 var dni= document.getElementById("dni_numero");
 var letra=document.getElementById("dni_letra");
-var dniprobable = dni + letra;
 
-var resto= Number(dni%23);
-var letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
-var dnicorrect =dni+letras[resto];
+var resto = Number(Number(dni.value) % Number(divisor));
+
+var dniprobable = String(Number(dni.value) + letra.value);
+
+
+var dnicorrect = String(Number(dni.value) + letras[resto]);
 
 var btn_submit = document.getElementById("verifica")
+
 btn_submit.addEventListener("click", function () {
    
     if(dniprobable===dnicorrect)
