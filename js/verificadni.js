c72df20a77729ef670bc787f353fa58e717735a3
@yeral-guerrero <gagl73@yahoo.com>
2020-06-04 19:52:03 +0200

    Verfica DNI

--- js/verificadni.js
+++ js/verificadni.js
@@ -1,8 +1,19 @@
-var dni=document.getElementById("dni");
-console.log(dni);
-console.log(dni.innerHTML)
 
-var btn_submit = document.getElementById("submit")
+var nombre01=document.getElementById("nombre");
+var dni= document.getElementById("dni_numero");
+var letra=document.getElementById("dni_letra");
+var dniprobable = dni + letra;
+
+var resto= Number(dni%23);
+var letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+var dnicorrect =dni+letras[resto];
+
+var btn_submit = document.getElementById("verifica")
 btn_submit.addEventListener("click", function () {
-    alert("DNI Incorrecto");
-  });
+   
+    if(dniprobable===dnicorrect)
+        alert("DNI Correcto Felicidades")
+    else
+        alert("DNI Incorrecto...Revise por favor!")
+});
+
